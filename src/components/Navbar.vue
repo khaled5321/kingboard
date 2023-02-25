2a60b0e759b2a47e55ba1d997fc73fad001b5fd5
Khaled Emad <khaledemad656@gmail.com>
2023-02-25 20:33:52 +0200

    init project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="container mx-auto p-6">
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
