1aaa7e61ec24125deeda007746a20c6b6bedd6a3
Khaled Emad <khaledemad656@gmail.com>
2023-02-27 17:29:23 +0200

    added hero section

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="bg-[#2d3b48]">
+    <header class="bg-[#2d3b48] drop-shadow-xl shadow-xl">
         <nav class="container flex mx-auto px-6 py-4 items-center">
             <RouterLink to="/" class="flex gap-1 items-center">
                 <img src="/pawn_logo.svg" alt="logo" class="w-12 md:w-16 drop-shadow-lg">
                 <h1 class="text-2xl text-white font-extrabold font-serif md:text-4xl drop-shadow-lg">KingBoard</h1> 
             </RouterLink>
 
-            <RouterLink to="/" class="mr-0 font-bold font-mono ml-auto p-2 bg-[#c9d8d3] rounded md:p-3 text-gray-900">
+            <RouterLink to="/" class="mr-0 drop-shadow-lg font-bold font-mono ml-auto p-2 bg-[#c9d8d3] rounded md:p-3 text-gray-900">
                 Login
             </RouterLink>
         </nav>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,12 @@
 </script>
 
 <template>
-  <main>
+  <main class="bg-[#c9d8d3]">
+    <div class="container flex flex-col mx-auto px-6 py-6 items-center">
+      <div class="flex flex-col items-center">
+        <h1 class="text-3xl text-center font-bold text-gray-900">Play Chess Online With Your Friends!</h1>
+        <button class="mt-7 px-5 py-3 text-white font-bold text-lg bg-gray-800 rounded">Play Online</button>
+      </div>
+    </div>
   </main>
 </template>
